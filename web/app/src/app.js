65b5cd73eb701f5920255ed9be5b50e700fb71f5
Chong <harimabf@gmail.com>
2016-01-11 16:53:27 +0800

    add nav banner

--- web/app/src/app.js
+++ web/app/src/app.js
@@ -1,35 +1,31 @@
-'use strict'
-/* App Module */
-var dalabApp = angular.module('dalabApp', [
-  'ngRoute',
-  'ngMaterial',
+(function() {
+  'use strict'
+  /* App Module */
+  var dalabApp = angular.module('dalabApp', [
+    'ngRoute',
+    'ngMaterial',
 
-  'people'
-]);
+    'people'
+  ]);
 
-dalabApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.when('/people', {
-      templateUrl: 'src/people/view/peopleList.html',
-      controller: 'PeopleController',
-      controllerAs: 'peopleCtrl'
-    }).otherwise({
-      redirectTo: '/people'
-    });
-  }
-]);
-
-dalabApp.config(['$mdThemingProvider', '$mdIconProvider',
-  function($mdThemingProvider, $mdIconProvider) {
-    $mdThemingProvider.theme('default')
-      .primaryPalette('cyan', {
-        'default': '500',
-        'hue-1': '100',
-        'hue-2': '600',
-        'hue-3': 'A100'
-      })
-      .accentPalette('pink', {
-        'default': '200'
+  dalabApp.config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.when('/people', {
+        templateUrl: 'src/people/view/peopleList.html',
+        controller: 'PeopleController',
+        controllerAs: 'peopleCtrl'
+      }).otherwise({
+        redirectTo: '/people'
       });
-  }
-]);
+    }
+  ]);
+
+  dalabApp.config(['$mdThemingProvider', '$mdIconProvider',
+    function($mdThemingProvider, $mdIconProvider) {
+      $mdThemingProvider.theme('default')
+        .primaryPalette('cyan')
+        .accentPalette('pink');
+    }
+  ]);
+
+})();
