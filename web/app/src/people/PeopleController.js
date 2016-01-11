5231bea706428d5618765b982d39b4ebded43e0f
Chong <harimabf@gmail.com>
2016-01-11 21:37:17 +0800

    reformat css and fix width problem

--- web/app/src/people/PeopleController.js
+++ web/app/src/people/PeopleController.js
@@ -7,20 +7,7 @@
   function PeopleController(People) {
     var self = this;
 
-    // group people by role
     self.roles = ['faculty', 'student'];
-    self.roleNames = {
-      'faculty': '教师',
-      'student': '学生'
-    };
-    self.people = {};
-    People.query(function(people) {
-      for (var i in self.roles) {
-        var role = self.roles[i];
-        self.people[role] = people.filter(function(p) {
-          return role == p.role;
-        });
-      }
-    });
+    self.people = People.query();
   }
 })();
