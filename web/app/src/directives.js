14d07cf87a716de014ee5c912536547da95c7967
Chong <harimabf@gmail.com>
2016-01-16 01:34:30 +0800

    implement nav button underline effect

--- web/app/src/directives.js
+++ web/app/src/directives.js
@@ -1,6 +1,25 @@
 (function() {
   var dalabApp = angular.module('dalabApp');
 
+  dalabApp.directive('mdPrimaryContrast', ['uiConfig',
+    function(uiConfig) {
+      return {
+        restrict: 'C',
+        scope: true,
+        link: function(scope, element, attrs) {
+          var el = $(element);
+          var primaryColor = uiConfig.color.primary;
+          var level = 'default';
+          el.css('background', '#' + primaryColor.palette[primaryColor[level]]);
+          var color = 'white';
+          if (primaryColor.palette['contrastDefaultColor'] == 'light' && $.inArray(primaryColor[level], primaryColor.palette['contrastDarkColors']) > -1 || primaryColor.palette['contrastDefaultColor'] == 'dark' && $.inArray(primaryColor[level], primaryColor.palette['contrastLightColors']) > -1)
+            color = 'black';
+          el.css('color', color);
+        }
+      }
+    }
+  ]);
+
   dalabApp.directive('daWindow', ['$window',
     function($window) {
       return {
@@ -31,22 +50,23 @@
     };
   }]);
 
-  dalabApp.directive('mdPrimaryContrast', ['uiConfig',
-    function(uiConfig) {
-      return {
-        restrict: 'C',
-        scope: true,
-        link: function(scope, element, attrs) {
-          var el = $(element);
-          var primaryColor = uiConfig.color.primary;
-          var level = 'default';
-          el.css('background', '#' + primaryColor.palette[primaryColor[level]]);
-          var color = 'white';
-          if (primaryColor.palette['contrastDefaultColor'] == 'light' && $.inArray(primaryColor[level], primaryColor.palette['contrastDarkColors']) > -1 || primaryColor.palette['contrastDefaultColor'] == 'dark' && $.inArray(primaryColor[level], primaryColor.palette['contrastLightColors']) > -1)
-            color = 'black';
-          el.css('color', color);
+  dalabApp.directive('daToolbarNavButton', ['uiConfig', function(uiConfig) {
+    return {
+      restrict: 'C',
+      scope: true,
+      link: function(scope, element, attrs) {
+        var el = $(element);
+        var primaryColor = uiConfig.color.primary;
+        var level = 'default';
+        el.css('border-top-color', '#' + primaryColor.palette[primaryColor[level]]);
+        var color = 'rgba(255, 255, 255, 0.5)'
+        scope.light = true;
+        if (primaryColor.palette['contrastDefaultColor'] == 'light' && $.inArray(primaryColor[level], primaryColor.palette['contrastDarkColors']) > -1 || primaryColor.palette['contrastDefaultColor'] == 'dark' && $.inArray(primaryColor[level], primaryColor.palette['contrastLightColors']) > -1) {
+          color = 'rgba(0, 0, 0, 0.5)';
+          scope.light = false;
         }
+      //  el.css('border-bottom-color', color);
       }
-    }
-  ]);
+    };
+  }]);
 })();
